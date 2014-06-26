65e2617bb23a52d7d023f2642088b2d55954f26b
ggholson <deviowa@Gregs-Macbook-Air.local>
2014-06-26 09:30:04 -0500

    Added timeouts and delete method

--- server.js
+++ server.js
@@ -2,16 +2,22 @@ var express = require('express');
 var fs = require('fs');
 var bodyParser = require('body-parser')
 var app = express();
+
 var positions = {};
+var timers = {};
+
 
 var create = function(req, res, next) {
-	var ip = req.ip.toString();
-    if(positions[ip]) {
-    	next();
-    }
-    else {
-    	positions[ip] = {"x":0,"y":0,"state":"red"};
-    	next();
+    var ip = req.ip.toString();
+    if (positions[ip]) {
+        next();
+    } else {
+        positions[ip] = {
+            "x": 0,
+            "y": 0,
+            "state": "red"
+        };
+        next();
     }
 }
 
@@ -25,14 +31,23 @@ app.get('/', function(req, res) {
     res.sendfile('index.html')
 });
 
-app.put('/positions/me', function (req, res) {
-	var ip = req.ip.toString();
-  	positions[ip].x += req.body.top;
-  	positions[ip].y += req.body.right;
-	var player = {'x': positions[ip].x,
-		'y': positions[ip].y,
-		'state': positions[ip].state.toString()};
-	res.send(JSON.stringify(player));
+app.put('/positions/me', function(req, res) {
+    var ip = req.ip.toString();
+
+    clearTimeout(timers[ip]);
+    timers[ip] = setTimeout(function() {
+        delete positions[ip];
+        delete timers[ip];
+    }, 2000);
+
+    positions[ip].x += req.body.top;
+    positions[ip].y += req.body.right;
+    var player = {
+        'x': positions[ip].x,
+        'y': positions[ip].y,
+        'state': positions[ip].state.toString()
+    };
+    res.send(JSON.stringify(player));
 });
 
 console.log("Starting to listen");
