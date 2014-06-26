85c440566c4dbb4cd4cbd09fba36e887d8205408
rlaake <robblaake@gmail.com>
2014-06-26 09:27:49 -0500

    added create function

--- server.js
+++ server.js
@@ -2,37 +2,37 @@ var express = require('express');
 var fs = require('fs');
 var bodyParser = require('body-parser')
 var app = express();
+var positions = {};
+
+var create = function(req, res, next) {
+	var ip = req.ip.toString();
+    if(positions[ip]) {
+    	next();
+    }
+    else {
+    	positions[ip] = {"x":0,"y":0,"state":"red"};
+    	next();
+    }
+}
 
 app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser());
 
+app.use(create);
+
 app.get('/', function(req, res) {
     res.sendfile('index.html')
 });
 
-app.put('/positions/me', function(req, res) {
+app.put('/positions/me', function (req, res) {
 	var ip = req.ip.toString();
-	fs.readFile('positions.json', function (err, data) {
-	    if (err) {
-	    	res.send(500);
-	    	return;
-	    }
-	    var dataitems=JSON.parse(data.toString());
-      	dataitems[ip].x += req.body.top;
-      	dataitems[ip].y += req.body.right;
-      	var newdata = JSON.stringify(dataitems);
-      	fs.writeFile('positions.json', newdata, function(err) {
-      		if (err) {
-      			res.send(500);
-      			return;
-      		}
-      		var player = {'x': dataitems[ip].x,
-      			'y': dataitems[ip].y,
-      			'state': dataitems[ip].state.toString()};
-      		res.send(JSON.stringify(player));
-      	});
-	});
+  	positions[ip].x += req.body.top;
+  	positions[ip].y += req.body.right;
+	var player = {'x': positions[ip].x,
+		'y': positions[ip].y,
+		'state': positions[ip].state.toString()};
+	res.send(JSON.stringify(player));
 });
 
 console.log("Starting to listen");
